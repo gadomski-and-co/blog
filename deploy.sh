d6bf46661759f66be4b3db146d9b8e3039129d9f
Benjamin S. Meyers <ben@lionlogic.org>
2017-09-12 10:34:59 -0400

    Minor tweaks. Still fighting with Jekyll.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
-/Library/Ruby/Gems/2.0.0/gems/jekyll-2.4.0/bin/jekyll build
+#/Library/Ruby/Gems/2.0.0/gems/jekyll-2.4.0/bin/jekyll build
+/var/lib/gems/2.4.0/gems/jekyll-3.5.2/exe/jekyll build
 
 zip -r _site.zip _site
 scp _site.zip gadomskico@nitron.se.rit.edu:~/
